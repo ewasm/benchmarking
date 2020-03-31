aaaf8e101669519c05675b88aec28e7b65f77557
Jared Wasinger <j-wasinger@hotmail.com>
2020-03-31 04:28:00 +0000

    fix formatting issue with evmone

--- evm/scripts/merge.py
+++ evm/scripts/merge.py
@@ -7,6 +7,20 @@ RESULT_CSV_OUTPUT_PATH = "/evmraceresults/"
 EVMS = ["evmone", "parity", "geth", "cita-vm"]
 RESULT_FILE = os.path.join(RESULT_CSV_OUTPUT_PATH + "evm_benchmarks.csv")
 
+# translate a string representing a numerical field like '1.8M' to '1800000'
+def format_evmone_benchmark(csv_line):
+    fields = csv_line.split(',')
+    gas_used = fields[-1]
+
+    if gas_used[-1].lower() == 'k':
+        gas_used = str(int(float(gas_used[:-1]) * 1000))
+    elif gas_used[-1].lower() == 'm':
+        gas_used = str(int(float(gas_used[:-1]) * 1000000))
+    else:
+        return csv_line
+
+    return ','.join(fields[:-1] + [gas_used])[:-1]
+
 # merge benchmarks from multiple engines into one csv output
 def main():
     merged_csv_contents = 'engine, test_name, total_time, gas_used\n'
@@ -21,7 +35,10 @@ def main():
         
     for i in range(1, len(evm_results[0])):
         for e in range(0, len(EVMS)):
-            merged_csv_contents += evm_results[e][i] + '\n'
+            if EVMS[e] == 'evmone':
+                merged_csv_contents += format_evmone_benchmark(evm_results[e][i]) + '\n'
+            else:
+                merged_csv_contents += evm_results[e][i] + '\n'
 
     with open(RESULT_FILE, 'w') as bench_result_file:
         bench_result_file.write(merged_csv_contents)
