7d801ae7666479059c7b228af469e2d25a736284
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2020-04-02 21:24:54 -0600

    print usage in run_precompiles_bench.sh

--- evm/scripts/run_precompiles_bench.sh
+++ evm/scripts/run_precompiles_bench.sh
@@ -1,6 +1,7 @@
-if [ $1 == 'geth' ] || [ $1 == 'parity' ]
+if [ "$1" == 'geth' ] || [ "$1" == 'parity' ]
 then
     docker run -v $(pwd)/evmraceresults:/evmraceresults -v $(pwd)/scripts:/scripts -it $1-bench /usr/bin/python3 /scripts/bench$1precompiles.py
 else
-    echo Only geth and parity are supported
+    echo Usage:
+    echo "  $0 <geth|parity>"
 fi
