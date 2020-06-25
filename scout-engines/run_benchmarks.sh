acde4abb22773f4b1b84998de46f39d0f2f09109
Jared Wasinger <j-wasinger@hotmail.com>
2020-06-25 20:48:50 +0000

    refactor scout engines dockerfile to use ewasm/bench-build-base

--- scout-engines/run_benchmarks.sh
+++ scout-engines/run_benchmarks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker run --privileged \
-       -v $(pwd)/benchmark_results_data:/benchmark_results_data -it scout-engines python3 scout_bignum_bench.py
+       -v $(pwd)/benchmark_results_data:/benchmark_results_data -it ewasm/scout-engines:1 python3 scout_bignum_bench.py
