be2fe3104a62707240701069f67451e9b70eaae1
Jared Wasinger <j-wasinger@hotmail.com>
2020-07-30 20:05:56 +0000

    scout-engines: mount script at runtime instead of building them into the container

--- scout-engines/run_benchmarks.sh
+++ scout-engines/run_benchmarks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 docker run --privileged \
-       -v $(pwd)/benchmark_results_data:/benchmark_results_data -it ewasm/scout-engines:1 python3 scout_bignum_bench.py
+       -v $(pwd)/benchmark_results_data:/benchmark_results_data \
+       -v $(pwd)/nanodurationpy.py:/benchscript/nanodurationpy.py \
+       -v $(pwd)/scout_bignum_bench.py:/benchscript/scout_bignum_bench.py \
+       -it ewasm/scout-engines:1 python3 scout_bignum_bench.py
