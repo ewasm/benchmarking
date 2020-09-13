25b9a47b4a2e47178bb2cabbeb3aa9d32778d4cf
Jared Wasinger <j-wasinger@hotmail.com>
2020-09-13 21:55:15 +0000

    works

--- evm/scripts/run_bench.sh
+++ evm/scripts/run_bench.sh
@@ -15,9 +15,8 @@ docker run --env PYTHONIOENCODING=UTF-8 -v $(pwd)/evmraceresults:/evmraceresults
 docker run --env PYTHONIOENCODING=UTF-8 -v $(pwd)/evmraceresults:/evmraceresults -v $(pwd)/scripts:/scripts -v $(pwd)/input_data:/input_data -it ewasm/cita-vm-bench:1 /usr/bin/python3 /scripts/benchevm.py cita-vm
 
 # Run evmone384 benchmarks
-docker run --env PYTHONIOENCODING=UTF-8 -v $(pwd)/evmraceresults:/evmraceresults -v $(pwd)/scripts:/scripts -it ewasm/evmone-evm384-bench:1 /usr/bin/python3 /scripts/bench_evm384.py
+docker run --env PYTHONIOENCODING=UTF-8 -v $(pwd)/evmraceresults:/evmraceresults -v $(pwd)/scripts:/scripts -it ewasm/evmone-evm384-bench:2 /usr/bin/python3 /scripts/bench_evm384.py
 
 # Merge benchmarks
 # TODO: just use python for merging? the docker image is only used to run python.
 docker run --env PYTHONIOENCODING=UTF-8 -v $(pwd)/../benchmark_results_data:/benchmark_results_data -v $(pwd)/evmraceresults:/evmraceresults -v $(pwd)/scripts:/scripts -it ewasm/cita-vm-bench:1 /usr/bin/python3 /scripts/merge.py
-
