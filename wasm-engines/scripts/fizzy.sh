9df06a04ece854ae862f06556b5308586bb925f5
Jared Wasinger <j-wasinger@hotmail.com>
2020-11-23 05:50:38 +0000

    fix fizzy

--- wasm-engines/scripts/fizzy.sh
+++ wasm-engines/scripts/fizzy.sh
@@ -5,6 +5,7 @@ set -e
 # $1 = function name
 # $2 = wasm file
 
+# CWD is the directory of this script
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Create input file
@@ -17,8 +18,8 @@ echo $'\n' >> testcase.inputs   # 6. Expected result
 echo $'\n' >> testcase.inputs   # 7. Expected memory
 
 # Copy wasm file
-cp $2 ./testcase.wasm
+cp $2 $CWD/testcase.wasm
 
-$CWD/fizzy-bench --benchmark_filter=fizzy/* --benchmark_color=false ./ | sed 's/\([0-9][0-9]*\)\s\([nm]s\)/\1\2/g'
+$CWD/fizzy-bench --benchmark_filter=fizzy/* --benchmark_color=false $CWD | sed 's/\([0-9][0-9]*\)\s\([nm]s\)/\1\2/g'
 
 rm -f testcase.inputs testcase.wasm
